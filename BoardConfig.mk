0631fb6d56b595aa51db4ca32f552891e41e0864
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-07-08 01:44:58 +0200

    BoardConfig: Update OTA target assertion list
     * Add SM-G530W product name
    
    Change-Id: I6374d1d33937c641f5f7083f596fada40832e617

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@ include device/samsung/gprimelte-common/BoardConfigCommon.mk
 LOCAL_PATH := device/samsung/gprimelte
 
 # Asserts
-TARGET_OTA_ASSERT_DEVICE := gprimelte,gprimeltecan,gprimeltetmo,gprimeltemtr
+TARGET_OTA_ASSERT_DEVICE := gprimelte,gprimeltecan,gprimeltetmo,gprimeltemtr,gprimeltevl
 
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_fortuna_canopen_defconfig
